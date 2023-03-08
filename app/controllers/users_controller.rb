04eaeb17f9f9ec0813c44ac45a4c9e87f3b47412
EC2 Default User <pankun0321@gmail.com>
2023-03-08 07:05:35 +0000

    8.2から

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,21 @@ class UsersController < ApplicationController
       render :new
     end
   end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # 更新に成功した場合の処理を記述します。
+      flash[:success] = "ユーザーの情報を更新しました。"
+      redirect_to @user
+    else
+      render :edit      
+    end
+  end
 
   private
 
